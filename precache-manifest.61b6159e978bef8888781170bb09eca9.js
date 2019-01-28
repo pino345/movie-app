1381641d272fbee3905e694ae202731352699b27
pino345 <pino345@me.com>
2019-01-28 15:35:46 +0900

    Updates

---
+++ precache-manifest.61b6159e978bef8888781170bb09eca9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a4a27000ba76122af42",
+    "url": "/movie-app/static/js/runtime~main.2a4a2700.js"
+  },
+  {
+    "revision": "bc7e984bccd870433875",
+    "url": "/movie-app/static/js/main.bc7e984b.chunk.js"
+  },
+  {
+    "revision": "d120fdf0ddba1865b329",
+    "url": "/movie-app/static/js/1.d120fdf0.chunk.js"
+  },
+  {
+    "revision": "bc7e984bccd870433875",
+    "url": "/movie-app/static/css/main.7b044fd6.chunk.css"
+  },
+  {
+    "revision": "902583b6d529943cc4dd9c4e882bdb97",
+    "url": "/movie-app/index.html"
+  }
+];
